ab4ad384dd411ddb18d95ca3f6ad50c9c40ef708
yashjawale <yash25.j@gmail.com>
2024-08-04 23:02:01 +0530

    Variables, strings & numbers

--- main.go
+++ main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	
+	// strings
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi" // cannot be used outside of a function
+	// fmt.Println(nameFour)
+
+	// integers
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var numOne int8 = 127
+	// var numTwo int8 = -128
+	// var numThree uint8 = 255
+
+	var scoreOne float32 = 43.1
+	var scoreTwo float64 = 54352343432.432432 // Default inferred type
+
+	fmt.Println(scoreOne, scoreTwo)
 }
