d8caeee7c9cb19931043881474448ec6f3e84496
Yash Jawale <yash25.j@gmail.com>
2024-08-19 21:24:40 +0530

    Pointers

--- main.go
+++ main.go
@@ -2,32 +2,23 @@ package main
 
 import "fmt"
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateName(x *string) {
+	*x = "wedge"
 }
 
 func main() {
-	// group A types -> strings, ints, bools, floats, arrays, structs
-	// Non pointer values
+	name := "tifa"
 
-	name := "John"
-	name = updateName(name)
+	// updateName(name)
+
+	// fmt.Println("Memory Address of name is: ", &name)
+
+	m := &name
+	fmt.Println("Memory Address: ", m)
+	fmt.Println("Value at Memory Address: ", *m)
+
+	fmt.Println(name)
+	updateName(m)
 	fmt.Println(name)
 
-	// group B types -> slices, maps, functions
-	// Pointer wrapper values
-	menu := map[string]float64{
-		"soup":  4.99,
-		"pie":   7.99,
-		"salad": 6.99,
-		"coffee": 1.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 }
