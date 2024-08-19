07031c2a8d080e6b4b2821d9db671e2c67109aed
Yash Jawale <yash25.j@gmail.com>
2024-08-19 21:09:13 +0530

    Maps

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
-	sayHello("Gopher")
-
-	for _, p := range points {
-		fmt.Println(p)
+	menu := map[string]float64{
+		"soup":  4.99,
+		"salad": 6.99,
+		"pasta": 8.99,
+		"pizza": 9.99,
 	}
 
-	// var score = 99.5 // This will not work
-	// Has to be declared outside of the main function at root level
+	fmt.Println(menu)
+	fmt.Println(menu["salad"])
 
-	showScore()
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		1234567890: "John",
+		9987654321: "Jane",
+		5894544530: "Joe",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[1234567890])
+
+	phonebook[1234567890] = "John Doe"
+	fmt.Println(phonebook)
+
+	phonebook[5894544530] = "Yoshi"
+	fmt.Println(phonebook)
 }
