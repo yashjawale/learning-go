8b7ad21c3244c7627662be2a274fbdf34c9ebc63
Yash Jawale <yash25.j@gmail.com>
2024-08-19 20:30:44 +0530

    Loops

--- main.go
+++ main.go
@@ -2,41 +2,34 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 )
 
-// Standard Library available at: https://golang.org/pkg/
-
 func main() {
-	greeting := "Hello, World!"
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("Value of x is:", x)
+	// 	x++
+	// }
 
-	fmt.Println(strings.Contains(greeting, "Hello!"))
-	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "World"))
-	fmt.Println(strings.Split(greeting, ",")) // Returns a slice of strings
-	// Prints type
-	fmt.Printf("Type: %T\n", strings.Split(greeting, ","))
-	
-	// Does not modify the original string
-	fmt.Println("Original string value: ", greeting)
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("Value of i is:", i)
+	// }
 
-	// Sort package
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	names := []string{"John", "Doe", "Jane", "Doe"}
 
-	sort.Ints(ages)
-	fmt.Println(ages) // Changes the original slice
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
 
-	index := sort.SearchInts(ages, 1) // Returns the index of the value if found
-	// If not found, returns the index where the value should be inserted
+	// for index, value := range names {
+	// 	fmt.Printf("The value at index %v is %v \n", index, value)
+	// }
 
-	fmt.Println(index)
+	for _, value := range names {
+		fmt.Printf("The value at is %v \n", value)
+		value = "new value" // Does not change the value in the slice
+		// Value is local copy of the value in the slice
+	}
 
-	names := []string{"Daniel", "Jenny", "Steven", "Alice", "John"}
-
-	sort.Strings(names)
 	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "Alice"))
 }
